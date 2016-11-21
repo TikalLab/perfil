8999c4eacbc711a54e22d5b86c62ae6968ccb8cd
Shahar Solomianik <shahar@gnift.com>
2016-11-21 15:20:50 +0200

    ajax widgets

---
+++ routes/widgets.js
@@ -0,0 +1,155 @@
+var express = require('express');
+var router = express.Router();
+var util = require('util');
+var config = require('config');
+var url = require('url');
+var async = require('async');
+var request = require('request');
+var _ = require('underscore');
+var moment = require('moment')
+// var github = require('../app_modules/github');
+var errorHandler = require('../app_modules/error');
+var github = require('../app_modules/github');
+var stackoverflow = require('../app_modules/stackoverflow');
+var linkedin = require('../app_modules/linkedin');
+var meetup = require('../app_modules/meetup');
+
+var users = require('../models/users');
+
+router.get('/github/basic-stats',function(req,res,next){
+	async.parallel([
+		function(callback){
+			github.getUser(req.session.user.github.access_token,function(err,githubUser){
+				callback(err,githubUser)
+			})
+		},
+		function(callback){
+			github.getReposCounts(req.session.user.github.access_token,function(err,githubReposCounts){
+				callback(err,githubReposCounts)
+			})
+		},
+	],function(err,results){
+		if(err){
+			errorHandler.error(req,res,next,err);
+		}else{
+			console.log('results are: %s',util.inspect(results,{depth:8}))
+			render(req,res,'widgets/github/basic-stats',{
+				github_user: results[0],
+				repos_counts: results[1],
+			})
+		}
+	})
+})
+
+router.get('/github/repos-tag-cloud',function(req,res,next){
+	async.parallel([
+		function(callback){
+			github.getLanguagesTagCloud(req.session.user.github.access_token,function(err,githubLanguagesTagCloud){
+				callback(err,githubLanguagesTagCloud)
+			})
+		},
+	],function(err,results){
+		if(err){
+			errorHandler.error(req,res,next,err);
+		}else{
+			console.log('results are: %s',util.inspect(results,{depth:8}))
+			render(req,res,'widgets/github/repos-tag-cloud',{
+				repos_tag_cloud: results[0],
+			})
+		}
+	})
+})
+
+router.get('/user',function(req,res,next){
+	console.log('user is: %s',util.inspect(req.session.user))
+	async.parallel([
+		function(callback){
+			github.getUser(req.session.user.github.access_token,function(err,githubUser){
+				callback(err,githubUser)
+			})
+		},
+		function(callback){
+			stackoverflow.getUser(req.session.user.stackoverflow.access_token,function(err,stackoverflowUser){
+				callback(err,stackoverflowUser)
+			})
+		},
+		function(callback){
+			linkedin.getUser(req.session.user.linkedin.access_token,function(err,linkedinUser){
+				callback(err,linkedinUser)
+			})
+		},
+		function(callback){
+			stackoverflow.getUserAnswers(req.session.user.stackoverflow.access_token,function(err,stackoverflowAnswers){
+				callback(err,stackoverflowAnswers)
+			})
+		},
+		function(callback){
+			stackoverflow.getUserQuestions(req.session.user.stackoverflow.access_token,function(err,stackoverflowQuestions){
+				callback(err,stackoverflowQuestions)
+			})
+		},
+		function(callback){
+			github.getReposCounts(req.session.user.github.access_token,function(err,githubReposCounts){
+				callback(err,githubReposCounts)
+			})
+		},
+		function(callback){
+			github.getLanguagesTagCloud(req.session.user.github.access_token,function(err,githubLanguagesTagCloud){
+				callback(err,githubLanguagesTagCloud)
+			})
+		},
+		function(callback){
+			meetup.getUserGroups(req.session.user.meetup.refresh_token,function(err,meetupGroups){
+				callback(err,meetupGroups)
+			})
+		},
+	],function(err,results){
+		if(err){
+			errorHandler.error(req,res,next,err);
+		}else{
+			console.log('results are: %s',util.inspect(results,{depth:8}))
+			render(req,res,'index/user',{
+				github: results[0],
+				stackoverflow: results[1],
+				linkedin: results[2],
+				stackoverflow_answers: results[3],
+				stackoverflow_questions: results[4],
+				github_repos_counts: results[5],
+				github_languages_tag_cloud: results[6],
+				meetup_groups: results[7]
+			})
+		}
+	})
+})
+
+
+function render(req,res,template,params){
+
+//	params.user = req.session.user;
+//	params.alert = req.session.alert;
+//	delete req.session.alert;
+
+	params.app = req.app;
+	params._ = _;
+	// params.us = us;
+	params.moment = moment;
+	params.config = config;
+	params.util = util;
+
+	// params.alertIcons = alertIcons;
+	// params.alert = req.session.alert;
+	// delete req.session.alert;
+
+	params.user = req.session.user;
+
+	if(!('active_page' in params)){
+		params.active_page = false;
+	}
+
+	if(!('isHomepage' in params)){
+		params.isHomepage = false;
+	}
+
+	res.render(template,params);
+}
+module.exports = router;
