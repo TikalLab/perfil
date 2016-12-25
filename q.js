a5db4c072edca2607bd99238230ad663737938f9
Shahar Solomianik <shahar@gnift.com>
2016-12-25 13:07:28 +0200

    single user

--- q.js
+++ q.js
@@ -9,25 +9,39 @@ var github = require('./app_modules/github')
 
 
 var users = db.get('users');
-users.find({},function(err,users){
+users.findOne({_id: process.argv[2]},function(err,user){
   if(err){
     console.log('err is %s',err)
   }else{
-    console.log('got the following users: %s',util.inspect(users))
-    async.each(users,function(user,callback){
-      github.saveUserCommitEvents(db,user,null,function(err){
-        if(err){
-          // ignore errs mo haha
-          // callback(err)
-          callback()
-        }else{
-          callback()
-        }
-      })
-    },function(err){
+    console.log('got the following user: %s',util.inspect(user))
+    github.saveUserCommitEvents(db,user,null,function(err){
       if(err){
-        console.log('err is %s',err)
+        console.log('err is: %s',err)
+      }else{
+        console.log('finsihed successfuly')
       }
     })
   }
 })
+// users.find({},function(err,users){
+//   if(err){
+//     console.log('err is %s',err)
+//   }else{
+//     console.log('got the following users: %s',util.inspect(users))
+//     async.each(users,function(user,callback){
+//       github.saveUserCommitEvents(db,user,null,function(err){
+//         if(err){
+//           // ignore errs mo haha
+//           // callback(err)
+//           callback()
+//         }else{
+//           callback()
+//         }
+//       })
+//     },function(err){
+//       if(err){
+//         console.log('err is %s',err)
+//       }
+//     })
+//   }
+// })
